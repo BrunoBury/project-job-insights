414b62a7d84c3878b4ba0da18236f05e2db81dfc
Bruno <bruno_bury@hotmail.com>
2023-11-23 18:59:07 -0300

    Implement the read method req02

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,15 +1,24 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path) -> List[Dict]:
+        with open(path, "r", newline="", encoding="utf-8") as file:
+            csv_reader = csv.DictReader(file)
+            self.jobs_list = list(csv_reader)
+        return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
         pass
 
     def filter_by_multiple_criteria(self) -> List[dict]:
         pass
+
+
+# instancia = ProcessJobs()
+# dado_lidos = instancia.read("data/jobs.csv")
+# print(dado_lidos)
